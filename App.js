93c8ff8fae3fa098666b2875e82d944053b43f67
Hanan Waqar <58631025+hananwaqar@users.noreply.github.com>
2024-09-27 11:25:48 +0400

    change yarn version

--- App.js
+++ App.js
@@ -1,29 +1,31 @@
-import React, { useEffect } from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import React, { useEffect } from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-import { usePlayer } from './hooks/usePlayer';
-import { useToken } from './hooks/useToken';
-import { ModalProvider } from './context/modal';
-import { PlayerProvider } from './context/player';
+import { usePlayer } from "./hooks/usePlayer";
+import { useToken } from "./hooks/useToken";
+import { ModalProvider } from "./context/modal";
+import { PlayerProvider } from "./context/player";
 
 import { LoginStackNavigation } from "./navigation/LoginStackNavigation";
-import { BottomTabNavigation } from './navigation/BottomTabNavigation';
-import SplashScreen from './screens/SplashScreen';
+import { BottomTabNavigation } from "./navigation/BottomTabNavigation";
+import SplashScreen from "./screens/SplashScreen";
 
 export default function App() {
   const { setupPlayer } = usePlayer();
   const { token } = useToken();
   const queryClient = new QueryClient();
 
-  useEffect(() => { setupPlayer(); }, []);
+  useEffect(() => {
+    setupPlayer();
+  }, []);
 
   return (
     <ModalProvider>
       <QueryClientProvider client={queryClient}>
         <NavigationContainer>
           <PlayerProvider>
-            {token === null ? <SplashScreen /> : token.length > 0 ? <BottomTabNavigation /> : <LoginStackNavigation />}
+            <BottomTabNavigation />
           </PlayerProvider>
         </NavigationContainer>
       </QueryClientProvider>
--- screens/LoginScreen/index.js
+++ screens/LoginScreen/index.js
@@ -1,19 +1,19 @@
-import React, {useContext} from 'react';
-import {SafeAreaView, View, Text, TouchableOpacity} from 'react-native';
+import React, { useContext } from "react";
+import { SafeAreaView, View, Text, TouchableOpacity } from "react-native";
 
-import Entypo from 'react-native-vector-icons/Entypo';
-import AntDesign from 'react-native-vector-icons/AntDesign';
-import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
-import styles from './styles';
-import colors from '../../utils/colors';
-import {loginStrings} from '../../utils/strings';
-import {spotifyAuthConfig} from '../../utils/constants';
+import Entypo from "react-native-vector-icons/Entypo";
+import AntDesign from "react-native-vector-icons/AntDesign";
+import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
+import styles from "./styles";
+import colors from "../../utils/colors";
+import { loginStrings } from "../../utils/strings";
+import { spotifyAuthConfig } from "../../utils/constants";
 
-import AsyncStorage from '@react-native-async-storage/async-storage';
-import {useNavigation} from '@react-navigation/native';
-import {authorize} from 'react-native-app-auth';
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { useNavigation } from "@react-navigation/native";
+import { authorize } from "react-native-app-auth";
 
-import {ModalContext} from '../../context/modal';
+import { ModalContext } from "../../context/modal";
 
 /**
  * Handles user login for the application.
@@ -21,21 +21,21 @@ import {ModalContext} from '../../context/modal';
  * @param {object} navigation - The navigation object used to navigate the user to the main screen.
  * @returns {void}
  */
-const onLogin = async navigation => {
+const onLogin = async (navigation) => {
   try {
     const result = await authorize(spotifyAuthConfig);
     if (result.accessToken) {
-      console.log('token: ' + result.accessToken);
+      console.log("token: " + result.accessToken);
       const expirationDate = new Date(
-        result.accessTokenExpirationDate,
+        result.accessTokenExpirationDate
       ).getTime();
-      await AsyncStorage.setItem('spotifyToken', result.accessToken);
-      await AsyncStorage.setItem('refreshToken', result.refreshToken);
+      await AsyncStorage.setItem("spotifyToken", result.accessToken);
+      await AsyncStorage.setItem("refreshToken", result.refreshToken);
       await AsyncStorage.setItem(
-        'tokenExpirationDate',
-        expirationDate.toString(),
+        "tokenExpirationDate",
+        expirationDate.toString()
       );
-      navigation.navigate('HomeStack');
+      navigation.navigate("HomeStack");
     }
   } catch (error) {
     console.log(JSON.stringify(error));
@@ -44,14 +44,14 @@ const onLogin = async navigation => {
 
 const LoginScreen = () => {
   const navigation = useNavigation();
-  const {openModal} = useContext(ModalContext);
+  const { openModal } = useContext(ModalContext);
 
   return (
     <SafeAreaView style={styles.background}>
       <View style={styles.logoView}>
         <Entypo name="spotify" size={90} color={colors.spotifyWhite}></Entypo>
-        <Text style={[{marginTop: 70}, styles.logoText]}>
-          {' '}
+        <Text style={[{ marginTop: 70 }, styles.logoText]}>
+          {" "}
           {loginStrings.loginString1} {loginStrings.loginString2}
         </Text>
       </View>
@@ -59,49 +59,63 @@ const LoginScreen = () => {
         <TouchableOpacity
           onPress={() => onLogin(navigation)}
           style={[
-            {backgroundColor: colors.spotifyGreen},
+            { backgroundColor: colors.spotifyGreen },
             styles.genericTouchableOpacity,
-          ]}>
-          <Text style={[styles.touchableOpacityText, {color: 'black'}]}>
-            {' '}
+          ]}
+        >
+          <Text style={[styles.touchableOpacityText, { color: "black" }]}>
+            {" "}
             {loginStrings.signInWithSpotify}
           </Text>
         </TouchableOpacity>
         <TouchableOpacity
           style={[
-            {marginTop: 15, borderColor: colors.spotifyWhite, borderWidth: 0.7},
+            {
+              marginTop: 15,
+              borderColor: colors.spotifyWhite,
+              borderWidth: 0.7,
+            },
             styles.genericTouchableOpacity,
           ]}
-          onPress={() => openModal()}>
+          onPress={() => openModal()}
+        >
           <AntDesign
             name="google"
             size={23}
-            color={colors.spotifyWhite}></AntDesign>
+            color={colors.spotifyWhite}
+          ></AntDesign>
           <Text style={styles.touchableOpacityText}>
-            {' '}
+            {" "}
             {loginStrings.signInWithGoogle}
           </Text>
         </TouchableOpacity>
         <TouchableOpacity
           style={[
-            {marginTop: 15, borderColor: colors.spotifyWhite, borderWidth: 0.7},
+            {
+              marginTop: 15,
+              borderColor: colors.spotifyWhite,
+              borderWidth: 0.7,
+            },
             styles.genericTouchableOpacity,
           ]}
-          onPress={() => openModal()}>
+          onPress={() => openModal()}
+        >
           <MaterialCommunityIcons
             name="facebook"
             size={26}
-            color={colors.spotifyWhite}></MaterialCommunityIcons>
+            color={colors.spotifyWhite}
+          ></MaterialCommunityIcons>
           <Text style={styles.touchableOpacityText}>
-            {' '}
+            {" "}
             {loginStrings.signInWithFacebook}
           </Text>
         </TouchableOpacity>
         <TouchableOpacity
           onPress={() => openModal()}
-          style={styles.genericTouchableOpacity}>
-          <Text style={[{paddingTop: 10}, styles.touchableOpacityText]}>
-            {' '}
+          style={styles.genericTouchableOpacity}
+        >
+          <Text style={[{ paddingTop: 10 }, styles.touchableOpacityText]}>
+            {" "}
             {loginStrings.signIn}
           </Text>
         </TouchableOpacity>
