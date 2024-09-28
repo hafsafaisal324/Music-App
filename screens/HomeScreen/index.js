b0c10ad73bafdd08240c87224ff9330e16295124
Hanan Waqar <58631025+hananwaqar@users.noreply.github.com>
2024-09-29 00:37:41 +0400

    fixed search data

--- screens/HomeScreen/index.js
+++ screens/HomeScreen/index.js
@@ -1,18 +1,18 @@
-import React from 'react';
-import { ScrollView, View } from 'react-native';
+import React from "react";
+import { ScrollView, View } from "react-native";
 
-import { carouselStrings } from '../../utils/strings';
-import { usePlaylist } from '../../hooks/usePlaylist';
-import { useAlbum } from '../../hooks/useAlbum';
-import { useRecentlyPlayed } from '../../hooks/useRecentlyPlayed';
-import { useMessage } from '../../hooks/useMessage';
+import { carouselStrings } from "../../utils/strings";
+import { usePlaylist } from "../../hooks/usePlaylist";
+import { useAlbum } from "../../hooks/useAlbum";
+import { useRecentlyPlayed } from "../../hooks/useRecentlyPlayed";
+import { useMessage } from "../../hooks/useMessage";
 
-import BottomPadding from '../../components/BottomPadding';
-import ScreenHeader from '../../components/Headers/ScreenHeader';
-import HomeGrid from '../../components/Grids/HomeGrid';
-import HorizontalCarousel from '../../components/HorizontalCarousels/HorizontalCarousel';
+import BottomPadding from "../../components/BottomPadding";
+import ScreenHeader from "../../components/Headers/ScreenHeader";
+import HomeGrid from "../../components/Grids/HomeGrid";
+import HorizontalCarousel from "../../components/HorizontalCarousels/HorizontalCarousel";
 
-import styles from './styles';
+import styles from "./styles";
 
 const HomeScreen = () => {
   const { message } = useMessage();
@@ -21,11 +21,17 @@ const HomeScreen = () => {
   const { featuredAlbums } = useAlbum().featured();
   return (
     <ScrollView style={styles.background}>
-      <ScreenHeader title={message} icon={'notifications-outline'} />
+      <ScreenHeader title={message} icon={"notifications-outline"} />
       <HomeGrid items={recentlyPlayed} />
       <View style={styles.carouselView}>
-        <HorizontalCarousel items={featuredPlaylists} title={carouselStrings.featuredPlaylists} />
-        <HorizontalCarousel items={featuredAlbums} title={carouselStrings.featuredAlbums} />
+        <HorizontalCarousel
+          items={featuredPlaylists}
+          title={carouselStrings.featuredPlaylists}
+        />
+        <HorizontalCarousel
+          items={featuredAlbums}
+          title={carouselStrings.featuredAlbums}
+        />
       </View>
       <BottomPadding />
     </ScrollView>
