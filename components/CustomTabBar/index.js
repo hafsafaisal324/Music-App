23b1b3aea8743bf49006eea81b5a50a82237d02a
Hanan Waqar <58631025+hananwaqar@users.noreply.github.com>
2024-10-01 00:44:14 +0400

    [FIX] bottom bar alignment

--- components/CustomTabBar/index.js
+++ components/CustomTabBar/index.js
@@ -1,28 +1,50 @@
-import React from 'react';
-import { Text, TouchableOpacity } from 'react-native';
-import { LinearGradient } from 'react-native-linear-gradient';
+import React from "react";
+import { Text, TouchableOpacity } from "react-native";
+import { LinearGradient } from "react-native-linear-gradient";
 
-import { exploreStrings, libraryStrings } from '../../utils/strings';
-import Ionicons from 'react-native-vector-icons/Ionicons';
-import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
-import colors from '../../utils/colors';
-import styles from './styles';
+import { exploreStrings, libraryStrings } from "../../utils/strings";
+import Ionicons from "react-native-vector-icons/Ionicons";
+import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
+import colors from "../../utils/colors";
+import styles from "./styles";
 
 const CustomTabBar = ({ state, descriptors, navigation }) => (
-  <LinearGradient colors={['transparent', colors.appBackground]} locations={[0, 0.75]} style={styles.bottomTabGradient}>
+  <LinearGradient
+    colors={["transparent", colors.appBackground]}
+    // locations={[0.75, 0]}
+    style={styles.bottomTabGradient}
+  >
     {state.routes.map((route, index) => {
       const isFocused = state.index === index;
       const { focusedIcon, unfocusedIcon } = descriptors[route.key].options;
 
       return (
-        <TouchableOpacity key={route.key} style={styles.touchableIcon} onPress={() => navigation.navigate(route.name)}>
+        <TouchableOpacity
+          key={route.key}
+          style={styles.touchableIcon}
+          onPress={() => navigation.navigate(route.name)}
+        >
           {route.name === exploreStrings.explore ? (
             <>
-              {isFocused && <MaterialCommunityIcons name={unfocusedIcon} size={13} style={styles.circleIcon} />}
-              <Ionicons name={focusedIcon} size={24} color={colors.spotifyWhite} />
+              {isFocused && (
+                <MaterialCommunityIcons
+                  name={unfocusedIcon}
+                  size={13}
+                  style={styles.circleIcon}
+                />
+              )}
+              <Ionicons
+                name={focusedIcon}
+                size={24}
+                color={colors.spotifyWhite}
+              />
             </>
           ) : (
-            <Ionicons name={isFocused ? focusedIcon : unfocusedIcon} size={24} color={colors.spotifyWhite} />
+            <Ionicons
+              name={isFocused ? focusedIcon : unfocusedIcon}
+              size={24}
+              color={colors.spotifyWhite}
+            />
           )}
           <Text style={styles.labelText}>{route.name}</Text>
         </TouchableOpacity>
--- components/CustomTabBar/styles.js
+++ components/CustomTabBar/styles.js
@@ -1,5 +1,5 @@
-import { StyleSheet } from 'react-native';
-import colors from '../../utils/colors';
+import { StyleSheet } from "react-native";
+import colors from "../../utils/colors";
 
 const styles = StyleSheet.create({
   /**
@@ -12,14 +12,14 @@ const styles = StyleSheet.create({
    * - bottom: 0 (positioned at the bottom)
    * - width: '100%' (width of 100% relative to the parent)
    * - zIndex: 1 (z-index for layering)
-  */
+   */
   bottomTabGradient: {
-    flexDirection: 'row',
+    flexDirection: "row",
     height: 40,
-    alignItems: 'center',
-    position: 'absolute',
-    bottom: 0,
-    width: '100%',
+    alignItems: "center",
+    position: "absolute",
+    bottom: 30,
+    width: "100%",
     zIndex: 1,
   },
   /**
@@ -27,17 +27,17 @@ const styles = StyleSheet.create({
    * Represents the style for a touchable icon with the following properties:
    * - flex: 1 (flexible size to fit available space)
    * - alignItems: 'center' (centered alignment of children)
-  */
+   */
   touchableIcon: {
     flex: 1,
-    alignItems: 'center',
+    alignItems: "center",
   },
   /**
    * Label Text style.
    * Represents the style for label text with the following properties:
    * - fontSize: 10 (font size of 10 units)
    * - color: colors.spotifyWhite (text color using the 'spotifyWhite' color from the 'colors' object)
-  */
+   */
   labelText: {
     fontSize: 10,
     color: colors.spotifyWhite,
@@ -49,13 +49,13 @@ const styles = StyleSheet.create({
    * - top: 4.5 (top position of 4.5 units)
    * - paddingRight: 3 (right padding of 3 units)
    * - color: colors.spotifyWhite (icon color using the 'spotifyWhite' color from the 'colors' object)
-  */
+   */
   circleIcon: {
-    position: 'absolute',
+    position: "absolute",
     top: 4.5,
     paddingRight: 3,
     color: colors.spotifyWhite,
-  }
+  },
 });
 
 export default styles;
